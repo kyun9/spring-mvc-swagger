38402d33726eedfd99c27acc030fc050b9538ed6
Hyun Kyung <gusl108@naver.com>
2020-12-29 13:55:22 +0900

    swagger 테스트

---
+++ src/main/java/com/prj/swagger/config/SwaggerConfig.java
@@ -0,0 +1,74 @@
+package com.prj.swagger.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket apiV1() {
+        version = "V1";
+        title = "victolee API " + version;
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.prj.swagger.controller"))
+                .paths(PathSelectors.ant("/v1/api/**"))
+                .build()
+                .apiInfo(apiInfo(title, version));
+
+    }
+
+    @Bean
+    public Docket apiV2() {
+        version = "V2";
+        title = "victolee API " + version;
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.victolee.swaggerexam.api.v2"))
+                .paths(PathSelectors.ant("/v2/api/**"))
+                .build()
+                .apiInfo(apiInfo(title, version));
+
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                "Swagger로 생성한 API Docs",
+                version,
+                "www.example.com",
+                new Contact("Contact Me", "www.example.com", "foo@example.com"),
+                "Licenses",
+                "www.example.com");
+    }
+
+	/** Swagger UI 를 Resource Handler 에 등록 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
---
+++ src/main/java/com/prj/swagger/controller/TestSwaggerController.java
@@ -0,0 +1,29 @@
+package com.prj.swagger.controller;
+
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Api(value = "BoardController V2")
+@RequestMapping("/v1/api")
+public class TestSwaggerController {
+
+	@ApiOperation(value = "exam", notes = "예제입니다.")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK !!"),
+			@ApiResponse(code = 500, message = "Internal Server Error !!"),
+			@ApiResponse(code = 404, message = "Not Found !!") })
+	@GetMapping(value = "/board")
+	public Map<String, String> selectOneBoard(
+			@ApiParam(value = "게시판번호", required = true, example = "1") @RequestParam String no) {
+		Map<String, String> result = new HashMap<>();
+		result.put("author", "victolee");
+		result.put("content", "V1 API 내용");
+		return result;
+	}
+}
